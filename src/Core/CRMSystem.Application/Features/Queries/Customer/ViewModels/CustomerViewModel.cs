5f8ee72d4baab251d5b927c75579ea12b590fdf3
Murs000 <129412232+Murs000@users.noreply.github.com>
2024-10-30 22:46:54 +0400

    App. Application Add Customer ViewModel.

---
+++ src/Core/CRMSystem.Application/Features/Queries/Customer/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,10 @@
+namespace CRMSystem.Application.Features.Queries.Customer.ViewModels;
+
+public class CustomerViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
